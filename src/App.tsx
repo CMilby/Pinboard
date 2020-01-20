986e23eab140c554d3a155b299ba3ac9c8b5df04
Milby <c-milby@ti.com>
2020-01-20 15:51:24 -0600

    Initial commit from Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+interface IAppProps {}
+
+interface IAppState {}
+
+export default class App extends Component<IAppProps, IAppState> {
+	render() {
+		return <div>test</div>;
+	}
+}
